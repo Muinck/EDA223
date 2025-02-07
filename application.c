c593c8ef75835e1d51452c54c9488b14ce6010b1
alfocarbou <alfocarbou@gmail.com>
2025-02-07 14:59:21 +0100

    constants for part 0 preliminaries added

--- application.c
+++ application.c
@@ -4,6 +4,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+const int array[] = {
+  0, 2, 4, 0, 0, 2, 4, 0, 
+  4, 5, 7, 4, 5, 7, 7, 9, 
+  7, 5, 4, 0, 7, 9, 7, 5, 
+  4, 0, 0, -5, 0, 0, -5, 0
+};
+const int per_array[] = {
+  2024, 1911, 1803, 1702, 1607, 1516, 1431, 1351,
+  1275, 1203, 1136, 1072, 1012,  955,  901,  851,
+   803,  758,  715,  675,  637,  601,  568,  536,
+   506
+};
+unsigned int freq_idx_2_arr(unsigned int in) {
+  return in + 10
+}
+
 typedef struct {
     Object super;
     char str_buff[128];
