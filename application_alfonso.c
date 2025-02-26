a9de8ac274cb02e07d7dd6730bae804af58ca955
19elio <evm2412@gmail.com>
2025-02-26 09:53:35 +0100

    part 1 up to step 3

--- application_alfonso.c
+++ application_alfonso.c
@@ -1,11 +1,3 @@
-//User guide:
-//Press 'v' to input the desired volume
-//Press 'm' to mute and again to unmute
-//Press 'u' and 'd' to increase and decrease distortion, respectively
-//Press 'q' to enable and disable deadlines
-//Press 'h' to change the frequency
-//Press 'p' to print the user guide
-
 #include "TinyTimber.h"
 #include "sciTinyTimber.h"
 #include "canTinyTimber.h"
@@ -31,7 +23,6 @@ const int per_array[] = {
    803,  758,  715,  675,  637,  601,  568,  536,
    506
 };
-
 unsigned int freq_idx_2_arr(unsigned int in) {
   return in + 10;
 }
@@ -85,7 +76,7 @@ void reader(App*, int);
 void receiver(App*, int);
 
 App app = initApp();
-DAC_obj obj_dac = initDAC();
+DAC_obj DAC_obj = initDAC();
 loop_load load = initload();
 Serial sci0 = initSerial(SCI_PORT0, &app, reader);
 
@@ -123,23 +114,23 @@ void empty_loop(loop_load *self, int unused){
 	else
 		SEND(USEC(1300), USEC(1300), self, empty_loop, 0);
 	
-//	self->l_count++;
-//	if (self->l_count==999){
-//		self->l_count = 0;
-//		self->l_exec_time[self->l_run] = USEC_OF(CURRENT_OFFSET())-self->l_tic;
-//		self->l_tic = CURRENT_OFFSET();
-//		self->l_totaltime = self->l_totaltime + self->l_exec_time[self->l_run];
-//		self->l_exec_time[self->l_run] = self->l_exec_time[self->l_run]/1000;
-//		if(self->l_exec_time[self->l_run]>self->l_maxtime)
-//			self->l_maxtime=self->l_exec_time[self->l_run];
-//		self->l_run++;
-//		print("Run: %d.\n",self->l_run);
-//		if(self->l_run==500){
-//			self->l_average = (self->l_totaltime/1000)/500;
-//			print("Average WCET is %d.\n",self->l_average);
-//			print("Maximum WCET is %d.\n",self->l_maxtime);
-//		}
-//	}
+	self->l_count++;
+	if (self->l_count==999){
+		self->l_count = 0;
+		self->l_exec_time[self->l_run] = USEC_OF(CURRENT_OFFSET())-self->l_tic;
+		self->l_tic = CURRENT_OFFSET();
+		self->l_totaltime = self->l_totaltime + self->l_exec_time[self->l_run];
+		self->l_exec_time[self->l_run] = self->l_exec_time[self->l_run]/1000;
+		if(self->l_exec_time[self->l_run]>self->l_maxtime)
+			self->l_maxtime=self->l_exec_time[self->l_run];
+		self->l_run++;
+		print("Run: %d.\n",self->l_run);
+		if(self->l_run==500){
+			self->l_average = (self->l_totaltime/1000)/500;
+			print("Average WCET is %d.\n",self->l_average);
+			print("Maximum WCET is %d.\n",self->l_maxtime);
+		}
+	}
 }
 
 void dac_deadline(DAC_obj *self, int unused){
@@ -187,10 +178,7 @@ void DAC_wr(DAC_obj *self, int play){ // 0 -> silent, 1 -> sound (setted volume)
     *DAC_wr_pointer = self->volume;
     next = 0;
   }
-  	if (self->d_deadline==0)
-		AFTER(USEC(self->period), self, DAC_wr, next);
-	else
-		SEND(USEC(self->period), USEC(100), self, DAC_wr, next);
+  AFTER(USEC(self->period), self, DAC_wr, next);
 }
 
 void reader(App *self, int c) {
@@ -203,20 +191,20 @@ void reader(App *self, int c) {
     print("Rcv: '%c'\n", c);
   }
   switch (c) {
-	case 'p':
-      print("p: shows this message\n",0);
-      print("m: mutes the DAC\n",0);
-      print("h: sets the frequency of the DAC in hz\n",0);
-      print("u: increases loop range by 500\n",0);
-      print("d: decreases loop range by 500\n",0);
-      print("<int>v: sets the volume to <int>\n",0);
-      print("<int>k: changes the playing key to <int>\n",0);
-      print("q: enables/disables deadlines\n",0);
-      print("<int>e: adds number <int> to the list and prints sum and median of the list\n",0);
-      print("f: history list erased\n",0);
+    case 'p':
+      print("p: shows this message\n");
+      print("m: mutes the DAC\n");
+      print("h: sets the frequency of the DAC in hz\n");
+      print("u: increases loop range by 500\n");
+      print("d: decreases loop range by 500\n");
+      print("<int>v: sets the volume to <int>\n");
+      print("<int>k: changes the playing key to <int>\n");
+      print("q: enables/disables deadlines\n");
+      print("<int>e: adds number <int> to the list and prints sum and median of the list\n");
+      print("f: history list erased\n");
       break;
     case 'm'://mute
-      DAC_mute(&obj_dac, 0);
+      DAC_mute(&DAC_obj, 0);
       break;
     case 'h'://press h to set the freq in heartz
       self->str_buff[self->str_index] = '\0';
@@ -226,15 +214,15 @@ void reader(App *self, int c) {
         bufferValue=0;
         print("Minimum freq is 0 heartzs",0);
       }
-      DAC_set_freq(&obj_dac, bufferValue);
+      DAC_set_freq(&DAC_obj, bufferValue);
       print("Setting DAC frequency to %d\n", bufferValue);
       break;
     case 'u'://press u to increase the loop range
-      bufferValue = add_loop_range(&load, 0);
+      bufferValue = add_loop_range(&empty_loop, 0);
       print("Increasing loop range to %d\n", bufferValue);
       break;
     case 'd'://press d to decreaset the loop range
-      bufferValue = sub_loop_range(&load, 0);
+      bufferValue = sub_loop_range(&empty_loop, 0);
       print("Decreasing loop range to %d\n", bufferValue);
       break;
     case 'v'://press v to confirm volume change
@@ -249,66 +237,66 @@ void reader(App *self, int c) {
         print("Maximum volume is 10",0);
       }
       print("Volume set to: %d\n", bufferValue);
-      DAC_set_vol(&obj_dac,bufferValue);
+      DAC_set_vol(&DAC_obj,bufferValue);
       break;
-  case 'k':
-    self->str_buff[self->str_index] = '\0';
-    self->str_index = 0;
-    bufferValue = atoi(self->str_buff);
+    case 'k':
+      self->str_buff[self->str_index] = '\0';
+      self->str_index = 0;
+      bufferValue = atoi(self->str_buff);
 
-    print("Key: %d\n", bufferValue);
+      print("Key: %d\n", bufferValue);
 
-    for(int i = 0; i < 32; i++){
-      print("%d", per_array[freq_idx_2_arr(melody_notes[i] + bufferValue)]);
-      if(i != 31)
-        print(", ",0);
-    }
-    print("\n", 0);
-    
-    break;
-  case 'q'://enable and disable deadlines
-    dac_deadline(&obj_dac, 0);
-    load_deadline(&load, 0);
-    break;
-  case 'e':
-    self->str_buff[self->str_index] = '\0';
-    self->str_index = 0;
-    bufferValue = atoi(self->str_buff);
-
-    self->int_buff[self->int_index++%3] = bufferValue;
-    self->int_count = (self->int_count >= 3) ? 3 : self->int_count+1;
-
-    sum = 0;
-    for (unsigned short int i = 0; i < self->int_count; i++){
-      sum += self->int_buff[i];
-    }
-    
-    if(self->int_count == 1){
-      median = sum;
-    }else if(self->int_count == 2){
-      median = sum/2;
-    }else{
-      if ((self->int_buff[0] >= self->int_buff[1] && self->int_buff[0] <= self->int_buff[2]) || (self->int_buff[0] <= self->int_buff[1] && self->int_buff[0] >= self->int_buff[2])) {
-          median = self->int_buff[0]; // self->int_buff[0] is the middle value
-      } else if ((self->int_buff[1] >= self->int_buff[0] && self->int_buff[1] <= self->int_buff[2]) || (self->int_buff[1] <= self->int_buff[0] && self->int_buff[1] >= self->int_buff[2])) {
-          median = self->int_buff[1]; // self->int_buff[1] is the middle value
-      } else {
-          median = self->int_buff[2]; // self->int_buff[2] is the middle value
+      for(int i = 0; i < 32; i++){
+        print("%d", per_array[freq_idx_2_arr(melody_notes[i] + bufferValue)]);
+        if(i != 31)
+          print(", ",0);
       }
-    }
-    print("Entered integer %d: ", bufferValue);
-    print("sum = %d, ", sum);
-    print("median = %d\n", median);
-    break;
-  case 'f':
-    self->str_index = 0;
-    self->int_index = 0;
-    self->int_count = 0;
-    print("The 3-history has been erased.\n",0);
-    break;
-  default:
-    self->str_buff[self->str_index++] = c;
-    break;
+      print("\n", 0);
+      
+      break;
+    case 'q'://enable and disable deadlines
+      dac_deadline(&obj_dac, 0);
+      load_deadline(&distort, 0);
+      break;
+    case 'e':
+      self->str_buff[self->str_index] = '\0';
+      self->str_index = 0;
+      bufferValue = atoi(self->str_buff);
+
+      self->int_buff[self->int_index++%3] = bufferValue;
+      self->int_count = (self->int_count >= 3) ? 3 : self->int_count+1;
+
+      sum = 0;
+      for (unsigned short int i = 0; i < self->int_count; i++){
+        sum += self->int_buff[i];
+      }
+      
+      if(self->int_count == 1){
+        median = sum;
+      }else if(self->int_count == 2){
+        median = sum/2;
+      }else{
+        if ((self->int_buff[0] >= self->int_buff[1] && self->int_buff[0] <= self->int_buff[2]) || (self->int_buff[0] <= self->int_buff[1] && self->int_buff[0] >= self->int_buff[2])) {
+            median = self->int_buff[0]; // self->int_buff[0] is the middle value
+        } else if ((self->int_buff[1] >= self->int_buff[0] && self->int_buff[1] <= self->int_buff[2]) || (self->int_buff[1] <= self->int_buff[0] && self->int_buff[1] >= self->int_buff[2])) {
+            median = self->int_buff[1]; // self->int_buff[1] is the middle value
+        } else {
+            median = self->int_buff[2]; // self->int_buff[2] is the middle value
+        }
+      }
+      print("Entered integer %d: ", bufferValue);
+      print("sum = %d, ", sum);
+      print("median = %d\n", median);
+      break;
+    case 'f':
+      self->str_index = 0;
+      self->int_index = 0;
+      self->int_count = 0;
+      print("The 3-history has been erased.\n",0);
+      break;
+    default:
+      self->str_buff[self->str_index++] = c;
+      break;
   }
 }
 
@@ -331,13 +319,13 @@ void startApp(App *self, int arg) {
   CAN_SEND(&can0, &msg);
 
   AFTER(USEC(1300), &load, empty_loop, 0);
-  AFTER(USEC(500), &obj_dac, DAC_wr, 1);
+  AFTER(USEC(500), &DAC_obj, DAC_wr, 1);
 
 }
 
 int main() {
-	INSTALL(&sci0, sci_interrupt, SCI_IRQ0);
+  INSTALL(&sci0, sci_interrupt, SCI_IRQ0);
 	INSTALL(&can0, can_interrupt, CAN_IRQ0);
-	TINYTIMBER(&app, startApp, 0);
+  TINYTIMBER(&app, startApp, 0);
   return 0;
 }
